ceaca212747888244b3da53af7337b56a4e584af
Adjetey_Ken <kenadjei20@gmail.com>
2022-09-18 22:04:39 +0000

    home page creation and update

---
+++ src/pages/home.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+
+export default function Home({ navigation }) {
+  return (
+    <View style={style.container}>
+      <View>
+        <TouchableOpacity
+          style={style.login_button}
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        >
+          <Text style={style.login_buttontxt}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={style.signup_button}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Signin");
+          }}
+        >
+          <Text style={style.signup_buttontxt}>Signin</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  login_button: {
+    backgroundColor: "purple",
+    borderWidth: 1,
+    width: 100,
+    height: 40,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 10,
+  },
+  login_buttontxt: {
+    color: "white",
+  },
+  signup_button: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signup_buttontxt: {
+    color: "black",
+  },
+});
