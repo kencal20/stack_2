679b65c887c4ec6906b4e35058c3b0f71d32d08b
Adjetey_Ken <kenadjei20@gmail.com>
2022-09-19 12:14:01 +0000

    right header addition

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "../pages/home";
 import Signin from "../pages/signin";
 import Login from "../pages/login";
+import { Text } from "react-native";
 
 const Stack = createStackNavigator();
 
@@ -12,15 +13,27 @@ export default function Navigation() {
       <Stack.Navigator
         screenOptions={{
           headerStyle: {
-            backgroundColor:"blue"
+            backgroundColor: "blue",
           },
-          headerTitleAlign: "center",
-          headerTintColor:'white'
+          headerTitleAlign: "left",
+          headerTintColor: "white",
         }}
       >
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Signin" component={Signin} />
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Signin"
+          component={Signin}
+          options={{
+            headerRight: () => <Text style={style.rightheader}>kenneth</Text>,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerRight: () => <Text style={style.rightheader}>kenneth</Text>,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
